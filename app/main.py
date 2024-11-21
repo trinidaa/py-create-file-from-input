b94a40239efc20eb6f024d4e5eb918080c574c1d
trinidaa <71452328+trinidaa@users.noreply.github.com>
2024-11-21 22:36:16 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def main():
     file_name = input("Enter name of the file: ")
     if not file_name.endswith(".txt"):
         file_name += ".txt"
-    with open(file_name, mode="a" ) as file:
+    with open(file_name, mode="a") as file:
         while True:
             user_input = input("Enter new line of content: ")
             if user_input.lower() == "stop":
